fb8d9fa0324aadaa3d411ee3c098dc3780f7427d
Manikanta <swamy.mani95@gmail.com>
2025-02-20 16:41:26 +0000

    data types in cpp

---
+++ data_types.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 12.334;
+    string c = "It is a character string";
+    bool d = true;
+    string myText = "Hello";
+
+    cout << "int: " << a << endl;
+    cout << "float: " << b << endl;
+    cout << "string: " << c << endl;
+    cout << "bool: " << d << endl;
+    cout << "myText: " << myText << endl;
+
+    return 0;
+}
+
