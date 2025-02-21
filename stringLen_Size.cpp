78c85e9f155019a4b52fa12964bd6e02683a00af
Manikanta <swamy.mani95@gmail.com>
2025-02-21 16:15:05 +0000

    length and size of the string

---
+++ stringLen_Size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+int main(){
+	std::string len = "find length of the string";
+	std::string size = "find size of the string";
+	std::cout<< "Length :" << len.length() << std::endl;
+	std::cout<< "Size :" << len.size() << std::endl;
+	return 0;
+}
+
